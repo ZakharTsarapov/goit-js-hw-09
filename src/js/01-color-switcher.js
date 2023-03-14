03a681a0727bb0debc3079392238bfbc11af40db
ZakharTsarapov <tsarapovs@gmail.com>
2023-03-14 11:52:47 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+let intervalId = null;
+const INTERVAL_DELAY = 1000;
+const refs = {
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+  bodyColor: document.querySelector('body'),
+};
+
+refs.startBtn.addEventListener('click', () => {
+  refs.startBtn.disabled = true;
+  intervalId = setInterval(() => {
+    let color = getRandomHexColor();
+    refs.bodyColor.style.background = color;
+  }, INTERVAL_DELAY);
+});
+
+refs.stopBtn.addEventListener('click', () => {
+  refs.startBtn.disabled = false;
+  clearInterval(intervalId);
+});
