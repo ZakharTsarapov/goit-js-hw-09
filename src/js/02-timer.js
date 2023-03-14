c0d1d7ee120ace412a0f38c7d4c9f28d1d0afd53
ZakharTsarapov <tsarapovs@gmail.com>
2023-03-14 21:54:48 +0200

    timer 02

---
+++ src/js/02-timer.js
@@ -0,0 +1,86 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
+
+const DELAY = 1000;
+
+const refs = {
+  datetimePicker: document.querySelector('#datetime-picker'),
+  startBtn: document.querySelector('[data-start]'),
+  daysValue: document.querySelector('[data-days]'),
+  hoursValue: document.querySelector('[data-hours]'),
+  minutesValue: document.querySelector('[data-minutes]'),
+  secondsValue: document.querySelector('[data-seconds]'),
+};
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+    onClose([selectedDates]) {
+        if (selectedDates < new Date()) {
+    refs.startBtn.disabled = true;
+    Notiflix.Notify.warning('Чертов мазафака накосячил в дате, выбери другую');
+  } else {
+    refs.startBtn.disabled = false;
+    refs.startBtn.addEventListener('click', () => {
+      startCounter(selectedDates);
+    });
+  }
+  },
+};
+
+flatpickr(refs.datetimePicker, options);
+
+function startCounter(selectedDates) {
+  const intervalId = setInterval(() => {
+    const currentTime = new Date();
+    const deltaTime = Math.floor(selectedDates - currentTime);
+    const time = convertMs(deltaTime);
+
+    if (deltaTime <= 0) {
+        clearInterval(intervalId);
+        resetValues();
+    } else {
+      padValue(time);
+    }
+  }, DELAY);
+}
+
+function padValue(time) {
+  refs.secondsValue.textContent = time.seconds.toString().padStart(2, '0');
+  refs.minutesValue.textContent = time.minutes.toString().padStart(2, '0');
+  refs.hoursValue.textContent = time.hours.toString().padStart(2, '0');
+  refs.daysValue.textContent = time.days.toString().padStart(2, '0');
+};
+
+function resetValues() {
+    refs.daysValue.textContent = '00';
+    refs.hoursValue.textContent = '00';
+    refs.minutesValue.textContent = '00';
+    refs.secondsValue.textContent = '00';
+};
+
+
+function convertMs(ms) {
+    const second = 1000;
+    
+    const minute = second * 60;
+    
+    const hour = minute * 60;
+    
+    const day = hour * 24;
+    
+
+    const days = Math.floor(ms / day);
+    
+    const hours = Math.floor((ms % day) / hour);
+    
+    const minutes = Math.floor(((ms % day) % hour) / minute);
+    
+    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+    
+
+    return { days, hours, minutes, seconds };
+}
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -6,3 +6,94 @@ function createPromise(position, delay) {
     // Reject
   }
 }
+
+
+
+
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
+
+const DELAY = 1000;
+
+const refs = {
+  datetimePicker: document.querySelector('#datetime-picker'),
+  startBtn: document.querySelector('[data-start]'),
+  daysValue: document.querySelector('[data-days]'),
+  hoursValue: document.querySelector('[data-hours]'),
+  minutesValue: document.querySelector('[data-minutes]'),
+  secondsValue: document.querySelector('[data-seconds]'),
+};
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (selectedDates < new Date()) {
+      refs.startBtn.disabled = true;
+      Notiflix.Notify.failure(
+        'Чертов мазафака накосячил в дате, выбери другую'
+      );
+    } else {
+      refs.startBtn.disabled = false;
+      refs.startBtn.addEventListener('click', () => {
+        startCounter(selectedDates);
+      });
+    }
+    console.log(selectedDates[0]);
+  },
+};
+
+function startCounter(selectedDates) {
+  const intervalId = setInterval(() => {
+    const currentTime = Date.now();
+    const deltaTime = selectedDates - currentTime;
+    const time = convertMs(deltaTime);
+
+    if (deltaTime <= 0) {
+      clearInterval(intervalId);
+    } else {
+      padValues(time);
+    }
+  }, DELAY);
+}
+
+function padValues(time) {
+  refs.secondsValue.textContent = formatedDifferenceInMs.seconds
+    .toString()
+    .padStart(2, '0');
+  refs.minutesValue.textContent = formatedDifferenceInMs.minutes
+    .toString()
+    .padStart(2, '0');
+  refs.hoursValue.textContent = formatedDifferenceInMs.hours
+    .toString()
+    .padStart(2, '0');
+  refs.daysValue.textContent = formatedDifferenceInMs.days
+    .toString()
+    .padStart(2, '0');
+}
+
+function resetValues() {
+  refs.daysValue.textContent = '00';
+  refs.hoursValue.textContent = '00';
+  refs.minutesValue.textContent = '00';
+  refs.secondsValue.textContent = '00';
+}
+
+flatpickr(refs.datetimePicker, options);
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
